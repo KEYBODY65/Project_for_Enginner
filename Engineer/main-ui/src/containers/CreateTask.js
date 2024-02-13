6bc6cfec7cb420c64f05253783bff51fb2591a91
1mpuryyyyy <andreev200624@gmail.com>
2024-02-13 12:59:29 +0300

    CreateTask.js промежуточный

--- Engineer/main-ui/src/containers/CreateTask.js
+++ Engineer/main-ui/src/containers/CreateTask.js
@@ -1,43 +1,79 @@
 import './static/Dashboard.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
-export default function CreateTask(){
+export default function CreateTask() {
     const [submittedTasks, setSubmittedTasks] = useState([]);
     const [tasks, setTasks] = useState(false);
+    const [csrfToken, setCsrfToken] = useState();
 
     function handleNewTaskClick() {
         setTasks(true);
     }
 
+    useEffect(() => {
+        axios
+            .get('/teacher/get_csrf')
+            .then(res => {
+                const csrfToken = res.data.csrfToken;
+                setCsrfToken(csrfToken);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }, []);
+
     function handleSubmit(event) {
         event.preventDefault();
         const newTask = {
-        // Ваши данные о задании (например, комментарий, вес и т.д.)
+            describe: document.getElementById('describe').value,
+            comments: document.getElementById('floatingTextarea').value,
+            weight: document.getElementById('input').value,
+            files: document.getElementById('formFileMultiple').files,
         };
-        setSubmittedTasks([...submittedTasks, newTask]); // Добавление задания в список
+        setSubmittedTasks(newTask); // Добавление задания в список
+        console.log(submittedTasks);
     }
+
     function sendTasksToServer() {
-      const url = ''; // Замените на ваш URL сервера
-      // Отправка POST-запроса на сервер с данными списка заданий
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(submittedTasks),
-      })
-        .then(response => response.json())
-        .then(data => {
-          // Данные успешно отправлены на сервер
-        })
-        .catch(error => {
-          console.log(error)
+        const url = '/teacher/new_task_data/'; // Замените на ваш URL сервера
+        const formData = new FormData();
+
+        submittedTasks.forEach((task, index) => {
+            Object.keys(task).forEach(key => {
+                if (key === 'files' && task[key] instanceof FileList) {
+                    // Если ключ - 'files' и значение является FileList (т.е. массивом файлов)
+                    Array.from(task[key]).forEach(file => {
+                        formData.append(`file_${index}_${file.name}`, file);
+                    });
+                } else {
+                    formData.append(`${key}_${index}`,task[key]);
+                }
+            });
         });
+        // Отправка POST-запроса на сервер с данными списка заданий
+        const config = {
+            headers: {
+                "Authorization": csrfToken,
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+        fetch(url, {
+            method: 'POST',
+            body: formData,
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Данные успешно отправлены на сервер
+            })
+            .catch(error => {
+                console.log(error)
+            });
     }
 
 
-    function renderTasks(){
-        return  (
+    function renderTasks() {
+        return (
             <div>
                 <div className="form-floating">
                     <input className="form-control" placeholder="Leave a comment here" id="describe" type='text'/>
@@ -45,7 +81,8 @@ export default function CreateTask(){
                     <br/>
                 </div>
                 <div className='form-floating'>
-                    <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea" style={{height: 100}}></textarea>
+                    <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea"
+                              style={{height: 100}}></textarea>
                     <label htmlFor="floatingTextarea">Comments</label>
                 </div>
                 <div className="mb-3">
@@ -58,38 +95,41 @@ export default function CreateTask(){
                 <br/>
             </div>
         );
-    };
-    return(
+    }
+
+    return (
         <div className='container'>
-            <h2 style={{textAlign: 'center',  marginTop: 20}} > Добавление заданий </h2>
-            <hr className="my-4" />
+            <h2 style={{textAlign: 'center', marginTop: 20}}> Добавление заданий </h2>
+            <hr className="my-4"/>
             <div className='new_test mb-3'>
                 <div className='d-flex'>
-                    <button className='btn btn-primary mt-2' onClick={handleNewTaskClick}  type='button'>
-                    +
+                    <button className='btn btn-primary mt-2' onClick={handleNewTaskClick} type='button'>
+                        +
                     </button>
                     <h5> Добавить </h5>
                 </div>
             </div>
             <div className='new_test'>
-                </div>
-            <form>
-
-                {tasks ?(
-                    <>
-                        {renderTasks() }
-                        <button type='submit' onClick={handleSubmit} className='btn btn-primary mt-2'> Отправить </button>
-                        <br/>
-                        <button className='btn btn-primary mt-2' onClick={sendTasksToServer}>
-                          Отправить задания на сервер
+            </div>
+            {tasks ? (
+                <>
+                    <form>
+                        <input type="hidden" name="csrfmiddlewaretoken" value={csrfToken}/>
+                        {renderTasks()}
+                        <button type='button' onClick={handleSubmit}
+                                className='btn btn-primary mt-2'> Сохранить
                         </button>
-                    </>
-                    ):(
-                    <h5>
-                        Нажмите <span style={{textDecorationLine: 'underline'}}>+</span>, что бы добавить задание
-                    </h5>
-                )}
-            </form>
+                        <br/>
+                    </form>
+                    <button className='btn btn-primary mt-2' onClick={sendTasksToServer}>
+                        Отправить задания на сервер
+                    </button>
+                </>
+            ) : (
+                <h5>
+                    Нажмите <span style={{textDecorationLine: 'underline'}}>+</span>, что бы добавить задание
+                </h5>
+            )}
         </div>
     );
 
